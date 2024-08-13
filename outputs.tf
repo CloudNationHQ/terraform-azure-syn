42c312c3c3ddbf4006fa4ba9d62a21439eaf8065
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2024-08-13 12:20:29 +0200

    feat: change output names to be more descriptive (#13)
    Co-authored-by: dkool <dkool@dkools-MacBook-Pro.local>

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "synapse_workspace" {
+output "workspace" {
   description = "contains all synapse workspace configuration"
   value       = azurerm_synapse_workspace.synapse_workspace
 }
 
-output "synapse_sql_pool" {
+output "sql_pools" {
   description = "contains all synapse sql pool configuration"
   value       = azurerm_synapse_sql_pool.synapse_sql_pool
 }
 
-output "synapse_spark_pool" {
+output "spark_pools" {
   description = "contains all synapse spark pool configuration"
   value       = azurerm_synapse_spark_pool.synapse_spark_pool
 }
